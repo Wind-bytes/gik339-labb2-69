5de5d92152eedbfb026fb7038583dd870fc0875a
Samuel Karlsson <samuel.karlsson00@gmail.com>
2025-11-27 13:20:04 +0100

    Test inte klart

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-function spawnHeart() {
-  const heart = document.createElement('div');
-  heart.className = 'heart';
-  heart.textContent = '❤';
-  const startX = Math.random() * window.innerWidth;
-  const duration = 3000 + Math.random() * 3000;
-  const size = 16 + Math.random() * 32;
 
-  heart.style.left = startX + 'px';
-  heart.style.animationDuration = duration + 'ms';
-  heart.style.fontSize = size + 'px';
 
-  document.body.appendChild(heart);
+// Hämtade element
+const myDiv = document.querySelector(".myDiv");
+const allTextInputs = document.querySelectorAll(".textfield");
+const removeBtn = document.getElementById("removeBtn");
+const divStyleCheckbox = document.getElementById("divStyle");
 
-  setTimeout(() => {
-    heart.remove();
-  }, duration);
+
+
+divStyleCheckbox.addEventListener("change", function() {
+    const chosenColor = colorInput.value;
+    myDiv.style.backgroundColor = chosenColor;
+});
+
+
+function handleInputEvent(e) {
+    console.log("Event triggered by:", e.target);
+    const inputName = e.target.name;
+    console.log("Input name:", inputName);
+
+    if (inputName === "content") {
+        myDiv.innerHTML = e.target.value;
+    }
 }
 
-setInterval(spawnHeart, 300);
+// Koppla funktionen till alla textfält (input-event när användaren skriver)
+allTextInputs.forEach(input => {
+    input.addEventListener("input", handleInputEvent);
+    input.addEventListener("blur", handleInputEvent); // även vid focus out
+});
+
+// --- Eventlyssnare för knappen ---
+// När knappen klickas ska div-elementet försvinna
+removeBtn.addEventListener("click", function() {
+    myDiv.style.display = "none"; // tar bort div från visning
+});
+
